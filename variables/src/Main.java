a618c3e835bfcc59a598afdbc35b1517515fccab
PisethMao <pisetmao2002@gmail.com>
2024-04-09 16:12:12 +0700

    first commit

---
+++ variables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        String name = "mea ah hong";
+        char leng = 'a';
+        float vat = 10;
+        double o = 10f;
+        long jmr = 1000000000;
+        boolean thork = true;
+        System.out.println(a);
+        System.out.println(name);
+        System.out.println(leng);
+        System.out.println(vat);
+        System.out.println(o);
+        System.out.println(jmr);
+        System.out.println(thork);
+    }
+}
